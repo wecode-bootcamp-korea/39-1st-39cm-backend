c296566446fda0b74a008c8b5dd51aca6e15f26d
LeeDOngKen <ldk2948@gmail.com>
2022-11-23 22:14:22 +0900

    한줄

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 const { userService } = require('../services');
 
 const loginRequired = async (req, res, next) => {
@@ -10,7 +11,7 @@ const loginRequired = async (req, res, next) => {
         return res.status(error.statusCode).json({ message: error.message });
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = await promisify(jwt.verify(token, process.env.JWT_SECRET));
 
     const user = await userService.getUserById(decoded.id);
 
